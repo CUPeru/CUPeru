82194639ed1eddb8867455e70c603f8d5cfb1805
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-04 13:31:29 -0600

    get to post for text route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root                 to: "sessions#new"
   post 'twilio/voice', to: 'twilio#voice'
-  get '/twilio/text',  to: 'twilio#text'
+  post '/twilio/text',  to: 'twilio#text'
   get '/login',        to: "home#show"
 end
