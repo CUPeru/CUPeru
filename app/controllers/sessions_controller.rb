bdcd67f08b3f10a21307f25d34559bd5d335a6a4
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-03 18:55:44 -0600

    color change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    redirect_to "welcome#show"
+    redirect_to "home#show"
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "sessions#new"
+  get '/login', to: "home#show"
 end
