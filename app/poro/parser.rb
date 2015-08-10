826c71aebe9a3b537ff8fa8dd652f3378a601ed8
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-10 14:53:41 -0600

    first parser implementaiton

---
+++ app/poro/parser.rb
@@ -0,0 +1,21 @@
+require_relative 'emergency'
+require_relative 'broadcast'
+
+class Parser
+  def initialize(message)
+    @message = message
+    parse_message
+  end
+
+  def parse_message
+    text = message.body.downcase
+    case text
+    when text.include?("broadcast")
+      Broadcast.new(@message)
+    when text.include?("emergency")
+      Emergency.new(@message)
+    when text.include?("symptoms")
+      Symptom.new(@message)
+    end
+  end
+end
