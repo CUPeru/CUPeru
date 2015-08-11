453f240ab64fc40422e13e1a04830094f0fc3d95
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 17:07:29 -0600

    only pass to registrator if applicable

--- app/poro/poros.rb
+++ app/poro/poros.rb
@@ -4,7 +4,7 @@ require_relative 'parser'
 class MessageHandler
   def route_message(message)
     save_message(message)
-    Registrator.new.register_by_number(message)
+    Registrator.new.register_by_number(message) if message.body.include?("register")
     Parser.new.parse_message(message)
   end
 
