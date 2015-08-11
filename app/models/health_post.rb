ce4631265326a397f5fe903fc83a01674ea8e40f
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 15:44:33 -0600

    random code after creation

--- app/models/health_post.rb
+++ app/models/health_post.rb
@@ -2,4 +2,11 @@ class HealthPost < ActiveRecord::Base
   belongs_to :health_center
   has_many :agents
   has_many :tecnicos
+
+  after_create :set_random_code
+
+  def set_random_code
+    random = rand(1000..9999).to_s
+    update_attributes(code: random)
+  end
 end
