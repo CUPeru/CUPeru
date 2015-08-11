6dcedb004f360fb348f9ea50e341fe7e17875046
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 13:50:42 -0600

    copy nokogiri from gitflub

--- app/poro/symptom.rb
+++ app/poro/symptom.rb
@@ -1,4 +1,11 @@
 class Symptom
   def initialize(message)
   end
+
+  def scrape_for_disease
+    client          = Hurley::Client.new("http://github.com/")
+    body            = client.get("#{nickname}").body
+    document        = Nokogiri::HTML(body)
+    contributions   = document.css(".contrib-number")
+  end
 end
