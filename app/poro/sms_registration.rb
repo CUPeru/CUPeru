aeacc73aaf344d4702965990503edc0967a59eea
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 15:45:01 -0600

    check for code for foreign key registration

--- app/poro/sms_registration.rb
+++ app/poro/sms_registration.rb
@@ -1,11 +1,17 @@
 class Registrator
   def register_by_number(message)
-    if Agent.where(phone_number: message.from).empty? && message.body.include?("agent123")
-      Agent.create!(phone_number: message.from)
-    elsif Tecnico.where(phone_number: message.from).empty? && message.body.include?("tecnico123")
-      Tecnico.create!(phone_number: message.from)
-    elsif HealthCenter.where(phone_number: message.from).empty? && message.body.include?("health123")
-      HealthCenter.create!(phone_number: message.from)
+    words = message.body.split(" ")
+    name = words[1]
+    message_code = words[2]
+    health_post = HealthPost.find_by(code: message_code)
+
+    if Agent.where(phone_number: message.from).empty?
+      health_post.agents.create!(phone_number: message.from, name: name)
+    elsif Tecnico.where(phone_number: message.from).empty? && message.body.include?("tecnico")
+      health_post.tecnicos.create!(phone_number: message.from, name: name)
+    elsif HealthPost.where(phone_number: message.from).empty? && message.body.include("post")
+      health_center = HealthCenter.find_by(code: message_code)
+      health_center.health_posts.create!(phone_number: message.from)
     else
       "Sorry, can't register that number"
     end
