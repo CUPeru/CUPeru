1b1691393dd6fa06af74094236112c81a1a5b18d
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 15:46:00 -0600

    random code generation for health center

--- app/models/health_center.rb
+++ app/models/health_center.rb
@@ -1,3 +1,10 @@
 class HealthCenter < ActiveRecord::Base
   has_many :health_posts
+
+  after_create :set_random_code
+
+  def set_random_code
+    random = rand(1000..9999).to_s
+    update_attributes(code: random)
+  end
 end
