2eb917fd8e2b31b1cc0c94557b805bfbb0b3216b
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 17:35:15 -0600

    refactor code smells

--- app/poro/sms_registration.rb
+++ app/poro/sms_registration.rb
@@ -1,18 +1,17 @@
 class Registrator
   def register_by_number(message)
     words = message.body.split(" ")
-    type, name, code = words[1], words[2], words[3]
+    type, name, code = words[1..3]
     health_post = HealthPost.find_by(code: code)
     health_center = HealthCenter.find_by(code: code)
 
-    if Agent.where(phone_number: message.from).empty? && type == "agent"
-      health_post.agents.create!(phone_number: message.from, name: name)
-    elsif Tecnico.where(phone_number: message.from).empty? && type == "tecnico"
-      health_post.tecnicos.create!(phone_number: message.from, name: name)
-    elsif HealthPost.where(phone_number: message.from).empty? && type == "post"
-      health_center.health_posts.create!(phone_number: message.from)
-    else
-      "Sorry, can't register that number"
+    case type
+    when "agent"
+      health_post.agents.create(phone_number: message.from, name: name)
+    when "tecnico"
+      health_post.tecnicos.create(phone_number: message.from, name: name)
+    when "post"
+      health_center.health_posts.create(phone_number: message.from)
     end
   end
 end
