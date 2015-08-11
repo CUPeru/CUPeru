fb9578f2de9254de917e6e62adf6294796e9f566
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 12:56:31 -0600

    add real registration specs

--- spec/poros/registration_spec.rb
+++ spec/poros/registration_spec.rb
@@ -2,14 +2,31 @@ require 'rails_helper'
 require_relative '../../app/poro/sms_registration.rb'
 
 describe Registrator do
-  xit 'registers agents' do
+  it 'registers agents' do
+    expect(Agent.count).to eq(0)
+
     message = double()
     message.stub(:from).and_return("1234567890")
     message.stub(:body).and_return("agent123")
-    agent = Registrator.new(message).register_by_number(message)
 
-    expect(agent).to eq(Agent)
-    expect(agent.phone_number).to eq(message.phone_number)
+    agent = Registrator.new.register_by_number(message)
+
+    expect(agent.class).to eq(Agent)
+    expect(Agent.count).to eq(1)
   end
+
+  it 'registers tecnicos' do
+    expect(Agent.count).to eq(0)
+
+    message = double()
+    message.stub(:from).and_return("1234567890")
+    message.stub(:body).and_return("tecnico123")
+
+    tecnico = Registrator.new.register_by_number(message)
+
+    expect(tecnico.class).to eq(Tecnico)
+    expect(Tecnico.count).to eq(1)
+  end
+
 end
 
