b768d619431066e09603b6df462937d2d23a0802
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 12:55:00 -0600

    create message router god poro

--- app/poro/poros.rb
+++ app/poro/poros.rb
@@ -1,5 +1,19 @@
 require_relative 'sms_registration'
 require_relative 'parser'
-require_relative 'emergency'
-require_relative 'broadcast'
-require_relative 'symptom'
+
+class MessageHandler
+  def route_message(message)
+    save_message(message)
+    Registrator.new.register_by_number(message)
+    Parser.new.parse_message(message)
+  end
+
+  def save_message(message)
+    Message.create(
+      body: message.body,
+      to: message.to,
+      from: message.from,
+      date_sent: message.date_created
+    )
+  end
+end
