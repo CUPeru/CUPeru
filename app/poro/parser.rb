58503c0bfbe5c8ecb50099ae880965c1d0c1f791
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-11 12:54:41 -0600

    update keyword if applicable

--- app/poro/parser.rb
+++ app/poro/parser.rb
@@ -3,19 +3,18 @@ require_relative 'broadcast'
 require_relative 'symptom'
 
 class Parser
-  def initialize(message)
-    @message = message
-    parse_message
-  end
-
-  def parse_message
-    text = @message.body.downcase
+  def parse_message(twilio_message)
+    text = twilio_message.body.downcase
+    db_message = Message.find_by(body: twilio_message.body)
     if text.include?("broadcast")
-      Broadcast.new(@message)
+      db_message.update_attributes(keyword: "Broadcast") if db_message
+      Broadcast.new.alert(twilio_message)
     elsif text.include?("emergency")
-      Emergency.new(@message)
+      db_message.update_attributes(keyword: "Emergency") if db_message
+      Emergency.new(twilio_message)
     elsif  text.include?("symptoms")
-      Symptom.new(@message)
+      db_message.update_attributes(keyword: "Symptom") if db_message
+      Symptom.new(twilio_message)
     else
       "I dunno what to do with this message"
     end
