34ea86ef9e4fc65a0cf61265a61a5f87254e185e
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 15:23:09 -0600

    better variable names

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def show
     messages_collection = Tecnico.messages + Agent.messages
     @messages = []
-    messages_collection.map {|arc| arc.map {|m| @messages << m}}
+    messages_collection.map { |ar_collection| ar_collection.map { |message| @messages << message } }
   end
 end
 
