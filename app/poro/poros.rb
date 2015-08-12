118a0ad71e54b90f49a781461a506ea4374fbb90
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 15:53:21 -0600

    downcase message for registration

--- app/poro/poros.rb
+++ app/poro/poros.rb
@@ -4,7 +4,7 @@ require_relative 'parser'
 class MessageHandler
   def route_message(message)
     save_message(message)
-    Registrator.new.register_by_number(message) if message.body.include?("register")
+    Registrator.new.register_by_number(message) if message.body.downcase.include?("register")
     Parser.new.parse_message(message)
   end
 
