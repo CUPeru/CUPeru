ddbc7dafd4181b259b56a372aaf20c0d0d596bb7
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 15:22:36 -0600

    refactor conditional using classes, update polymorphic attributes

--- app/poro/parser.rb
+++ app/poro/parser.rb
@@ -4,19 +4,26 @@ require_relative 'symptom'
 
 class Parser
   def parse_message(twilio_message)
+    db_message(twilio_message)
     text = twilio_message.body.downcase
-    db_message = Message.find_by(body: twilio_message.body)
-    if text.include?("broadcast")
-      db_message.update_attributes(keyword: "Broadcast") if db_message
-      Broadcast.new.alert(twilio_message)
-    elsif text.include?("emergency")
-      db_message.update_attributes(keyword: "Emergency") if db_message
-      Emergency.new(twilio_message)
-    elsif  text.include?("symptoms")
-      db_message.update_attributes(keyword: "Symptom") if db_message
-      Symptom.new(twilio_message)
-    else
-      "I dunno what to do with this message"
+    [Broadcast, Emergency, Symptom].each do |poro|
+      if text.include?(poro.to_s.downcase)
+        @db_message.update_attributes(keyword: poro.to_s) if @db_message
+        poro.new(twilio_message)
+      end
     end
+  end
+
+  def find_worker(message)
+    agent   = Agent.find_by(phone_number: message.from)
+    tecnico = Tecnico.find_by(phone_number: message.from)
+    return agent   if agent
+    return tecnico if tecnico
+  end
+
+  def db_message(twilio_message)
+    worker      = find_worker(twilio_message)
+    @db_message = Message.find_by(body: twilio_message.body)
+    @db_message.update_attributes(messageable_id: worker.id, messageable_type: worker.class) if worker
   end
 end
