b681aa94eb093a2b4de73697c8cff79cd6243a3a
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 13:56:41 -0600

    worker show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root                          to: 'home#show'
+  get '/dashboard',             to: 'dashboard#show'
   post 'twilio/voice',          to: 'twilio#voice'
   post '/twilio/text',          to: 'twilio#text'
   post 'twilio/send_text',      to: 'twilio#send_text'
-  get '/auth/twitter',          as: '/login', to: 'sessions#new'
-  get '/auth/twitter/callback', to: 'sessions#create'
   get '/logout',                to: 'sessions#destroy'
-  get '/dashboard',             to: 'dashboard#show'
+  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/auth/twitter',          as: '/login',          to: 'sessions#new'
+  get '/agents/:id',            as: 'agent',          to: 'agents#show'
+  get '/tecnicos/:id',          as: 'tecnico',        to: 'tecnicos#show'
 end
