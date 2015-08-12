01c01be4d1e66e9ac179d2590c1e3d235abc4e19
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 11:34:17 -0600

    add all messages and polymorphic mesasage

--- app/models/tecnico.rb
+++ app/models/tecnico.rb
@@ -1,4 +1,9 @@
 class Tecnico < ActiveRecord::Base
   belongs_to :health_post
+  has_many :messages, as: :messageable
   validates_uniqueness_of :phone_number
+
+  def self.messages
+    all.map(&:messages)
+  end
 end
