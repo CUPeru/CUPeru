9950e00bb30b8815e3c9cc647b70f4886dd543a9
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-13 09:31:32 -0600

    refactor out smaller methods

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,13 @@
 class DashboardController < ApplicationController
   def show
+    @messages = aggregate_messages
+  end
+
+  private
+
+  def aggregate_messages
     messages_collection = Tecnico.messages + Agent.messages
-    @messages = []
-    messages_collection.map { |ar_collection| ar_collection.map { |message| @messages << message } }
+    messages_collection.flat_map { |ar_collection| ar_collection.flat_map { |message| message } }
   end
 end
 
