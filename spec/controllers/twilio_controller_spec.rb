f20d406757eeb1b6ad89760599eb435cbc47997a
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-12 19:55:52 -0600

    fix broken test

--- spec/controllers/twilio_controller_spec.rb
+++ spec/controllers/twilio_controller_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 describe TwilioController do
   context'#text' do
     it 'renders the valid twiml' do
-      Message.create(body: 'I am a sample text', to: '+15555555555', from: '+15555555555', date_sent: "Thu, 06 Aug 2015 16:13:31 +0000")
+      health_post = HealthPost.create
+      health_post.update_attributes(code: "9599")
+      agent = Agent.create(health_post_id: health_post.id)
+      message = agent.messages.create(body: 'I am a sample text', to: '+15555555555', from: '+15555555555', date_sent: "Thu, 06 Aug 2015 16:13:31 +0000")
       xml_type = "<Message>"
 
-      post :text, format: :xml
+      post :text, format: :xml, message: message.body
 
       expect(response).to have_http_status(:ok)
       expect(Message.count).to eq(2)
