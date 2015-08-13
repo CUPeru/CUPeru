560f78eef67c48264f360bd1e20175e2910c4f67
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-13 09:32:03 -0600

    comment out unfinished web scraping

--- app/poro/symptom.rb
+++ app/poro/symptom.rb
@@ -2,10 +2,10 @@ class Symptom
   def initialize(message)
   end
 
-  def scrape_for_disease
-    client          = Hurley::Client.new("http://github.com/")
-    body            = client.get("#{nickname}").body
-    document        = Nokogiri::HTML(body)
-    contributions   = document.css(".contrib-number")
-  end
+  #def scrape_for_disease
+  #  client          = Hurley::Client.new("http://github.com/")
+  #  body            = client.get("#{nickname}").body
+  #  document        = Nokogiri::HTML(body)
+  #  contributions   = document.css(".contrib-number")
+  #end
 end
