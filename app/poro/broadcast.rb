e2b0a380113b683c5b69d29d3f75efead42f8027
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-13 18:24:03 -0600

    hacky fix

--- app/poro/broadcast.rb
+++ app/poro/broadcast.rb
@@ -6,14 +6,25 @@ class Broadcast
   end
 
   def alert(message)
-    if find_health_center_by_message(message)
-      create_client
-      agents.each do |agent|
-          @account.messages.create(
-            from: "+12674227124",
-            to: agent.phone_number,
-            body: message.body
-          )
+    #if find_health_center_by_message(message)
+    #create_client
+    #agents.each do |agent|
+    #    @account.messages.create(
+    #      from: "+12674227124",
+    #      to: agent.phone_number,
+    #      body: message.body
+    #    )
+    #end
+    #end
+    if find_health_post(message)
+      @client ||= Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
+      @account = @client.account
+      find_health_post(message).agents.each do |agent|
+        @account.messages.create(
+          from: "+12674227124",
+          to: agent.phone_number,
+          body: message.body
+        )
       end
     end
   end
@@ -31,5 +42,9 @@ class Broadcast
 
   def agents
     HealthPost.where(health_center_id: @health_center.id).map { |post| post.agents }.first
+  end
+
+  def find_health_post(message)
+    HealthPost.find_by(phone_number: "+12154106125")
   end
 end
